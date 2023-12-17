87c0d5fc2af8be8953e6a7f4d1f9acb3455a6690
gigobyte <gigobest2@gmail.com>
2023-12-17 22:35:08 +0200

    Day 16 part 2

--- 16-js/index.mjs
+++ 16-js/index.mjs
@@ -18,9 +18,9 @@ const leftMirrorMap = { right: 'down', left: 'up', up: 'left', down: 'right' }
 
 const getEnergizedTiles = (
   grid,
-  visited = [],
   position = [0, 0],
   direction = 'right',
+  visited = [],
   energized = []
 ) => {
   if (visited.includes([position, direction].toString())) {
@@ -34,15 +34,15 @@ const getEnergizedTiles = (
     visited.push([position, direction].toString())
 
     const continued = (dir) =>
-      getEnergizedTiles(grid, visited, advance(position, dir), dir, energized)
+      getEnergizedTiles(grid, advance(position, dir), dir, visited, energized)
 
     const splitInto = (dir1, dir2) =>
       energized
         .concat(
-          getEnergizedTiles(grid, visited, advance(position, dir1), dir1, [])
+          getEnergizedTiles(grid, advance(position, dir1), dir1, visited, [])
         )
         .concat(
-          getEnergizedTiles(grid, visited, advance(position, dir2), dir2, [])
+          getEnergizedTiles(grid, advance(position, dir2), dir2, visited, [])
         )
 
     if (tile === '.') {
@@ -73,7 +73,7 @@ const getEnergizedTiles = (
   }
 }
 
-const solve1 = (input) => {
+const makeGrid = (input) => {
   const lines = input.replaceAll(/\r/g, '').split('\n')
 
   const grid = {}
@@ -84,10 +84,51 @@ const solve1 = (input) => {
     }
   }
 
-  return [...new Set(getEnergizedTiles(grid))].length
+  return grid
 }
 
-const solve2 = (input) => {}
+const countUniqueEnergizedTiles = (tiles) => [...new Set(tiles)].length
+
+const solve1 = (input) => {
+  const grid = makeGrid(input)
+  return countUniqueEnergizedTiles(getEnergizedTiles(grid))
+}
+
+const solve2 = (input) => {
+  const lines = input.replaceAll(/\r/g, '').split('\n')
+  const grid = makeGrid(input)
+  const tilesFromEachRun = []
+
+  for (let i = 0; i < lines.length; i++) {
+    tilesFromEachRun.push(
+      countUniqueEnergizedTiles(getEnergizedTiles(grid, [i, 0], 'right'))
+    )
+  }
+
+  for (let i = 0; i < lines[0].length; i++) {
+    tilesFromEachRun.push(
+      countUniqueEnergizedTiles(getEnergizedTiles(grid, [0, i], 'down'))
+    )
+  }
+
+  for (let i = 0; i < lines.length; i++) {
+    tilesFromEachRun.push(
+      countUniqueEnergizedTiles(
+        getEnergizedTiles(grid, [i, lines[0].length - 1], 'left')
+      )
+    )
+  }
+
+  for (let i = 0; i < lines[0].length; i++) {
+    tilesFromEachRun.push(
+      countUniqueEnergizedTiles(
+        getEnergizedTiles(grid, [lines.length - 1, i], 'up')
+      )
+    )
+  }
+
+  return Math.max(...tilesFromEachRun)
+}
 
 const input = fs.readFileSync('./input.txt').toString()
 
@@ -95,9 +136,9 @@ const start = performance.now()
 const result1 = solve1(input)
 const end = performance.now()
 
-// const start2 = performance.now()
-// const result2 = solve2(input)
-// const end2 = performance.now()
+const start2 = performance.now()
+const result2 = solve2(input)
+const end2 = performance.now()
 
 console.log(`Result 1: ${result1}. Execution time: ${end - start} ms`)
-// console.log(`Result 2: ${result2}. Execution time: ${end2 - start2} ms`)
+console.log(`Result 2: ${result2}. Execution time: ${end2 - start2} ms`)
